dd967dc06942ecb517840717832d7d6460e7be0e
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-12-20 21:19:09 -0300

    atualização imports

--- src/main/java/com/douglas/developer/album/controller/AlbumController.java
+++ src/main/java/com/douglas/developer/album/controller/AlbumController.java
@@ -2,7 +2,6 @@ package com.douglas.developer.album.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
